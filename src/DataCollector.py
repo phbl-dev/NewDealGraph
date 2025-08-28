eb5912bd34a3e6e0eb71daccadd35d76d5723bb0
Philip Boesen Blomholt <phbl@itu.dk>
2025-08-28 12:11:49 +0200

    Final?

--- src/DataCollector.py
+++ src/DataCollector.py
@@ -15,25 +15,34 @@ class DataCollector:
             url = f"https://node-api.fundmarket.dk/funds/ninfo/{fund_id}"
             response = requests.get(url, timeout=1000)
             data = response.json()["returns"]
+            
+            print(data)
             return data
-        except  TimeoutError:
+        except TimeoutError:
             return None
+
     @staticmethod
     def get_data_yf(fund_id: str) -> List[FundReturn]:
         """Hent data omkring fonde understøttet af yfinance API"""
-        data = yf.Ticker(fund_id).history(
-            start="2023-9-22", end=str(date.today()), actions=True
+        data = yf.download(
+            fund_id,
+            start="2023-9-22",
+            end=str(date.today()),
+            auto_adjust=False,
+            actions=True,
         )
 
         data = data.reset_index()
-
+        data.columns = ['_'.join(col).strip() if isinstance(col, tuple) else col for col in data.columns]
         nasdaq_cleaned = [
             {
-                "org_date": row["Date"].date().isoformat(),
-                "nav": row["Close"],
-                "dividend": row["Dividends"],
-                "value": row["Close"],
+                "org_date": row["Date_"].date().isoformat(),  # giver "2023-09-22"
+                "nav": row["Close_QQQ"],
+                "dividend": row["Dividends_QQQ"],
+                "value": row["Close_QQQ"],
+                "adjusted": row["Adj Close_QQQ"],
             }
             for _, row in data.iterrows()
         ]
+
         return nasdaq_cleaned
